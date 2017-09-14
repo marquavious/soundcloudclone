34adaf5042897d8082856fdfd5544e42e92da6ae
Marquavious Draggon <marq.draggon@gmail.com>
2017-09-13 20:33:02 -0700

    init

---
+++ soundcloud_challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soundcloud_challenge
+//
+//  Created by Marquavious on 9/13/17.
+//  Copyright © 2017 Marquavious Draggon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
